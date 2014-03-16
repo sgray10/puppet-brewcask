bb4fd845a5d43104105a11fbfd259c4f6ea1829e
Stephen Gray <stephen@grays.io>
2014-03-16 02:30:53 -0400

    tracing

--- lib/puppet/provider/package/brewcask.rb
+++ lib/puppet/provider/package/brewcask.rb
@@ -18,7 +18,9 @@ Puppet::Type.type(:package).provide :brewcask,
   end
 
   def self.run(*cmds)
+    puts "run: entering"
     command = ["sudo", "-E", "-u", Facter[:luser].value, "#{home}/bin/brew", "cask", *cmds].flatten.join(' ')
+    puts "command: #{command}"
     output = `#{command}`
     unless $? == 0
       fail "Failed running #{command}"
@@ -28,28 +30,35 @@ Puppet::Type.type(:package).provide :brewcask,
   end
 
   def self.current(name)
+    puts "current: entering"
     caskdir = Pathname.new "#{home}/Caskroom/#{name}"
+    puts "caskdir: #{caskdir}"
     caskdir.directory? && caskdir.children.size >= 1 && caskdir.children.sort.last.to_s
   end
 
   def query
+    puts "query: entering"
     return unless version = self.class.current(resource[:name])
     { :ensure => version, :name => resource[:name] }
   end
 
   def install
+    puts "install: entering"
     run "install", resource[:name], *install_options
   end
 
   def uninstall
-    run "uninstall", resource[:name]
+    puts "install: entering"
+    run  "uninstall", resource[:name]
   end
 
   def install_options
+    puts "install_options: entering"
     Array(resource[:install_options]).flatten.compact
   end
 
   def run(*cmds)
+    puts "run2: entering"
     self.class.run(*cmds)
   end
 end
